adb097ea8d883dec3b57e5b4bd2baa06711cd3e5
SparkCoder <abhishekdinesh3@gmail.com>
2023-03-19 15:59:46 +0530

    Added basic flask server

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
-from core import Module
-from core.submodule import SubModule
+from flask import Flask
+
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 
 def main() -> None:
-    pass
+    # Server start
+    port = int(os.environ.get('PORT', 8080))
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=port)
 
 
 if __name__ == '__main__':
--- core/submodule/submodule.py
+++
@@ -1,2 +0,0 @@
-class SubModule:
-    pass
