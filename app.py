3c391a69760dcd702d225bf04c59af7b6284843e
Abishek Jayan <abishekjayan98@gmail.com>
2023-04-06 15:54:19 +0530

    Implemented basic folder scanning functionality

--- app.py
+++ app.py
@@ -16,28 +16,19 @@ API_KEY = ""
 def n3twatch_homepage():
     return '<p>Hello, World!</p>', 200
 
-
-@app.route("/dwn", methods=["POST"])
+@app.route("/dwnld_state", methods=["POST"])
 def n3twatch_download_catcher():
-    if 'path' in request.json:
-        if path := request.json['path']:
-            print("New File Downloaded:", path)
-            with open(path, 'rb') as file:
-                headers = {
-                    "accept": "application/json",
-                    "x-apikey": API_KEY
-                }
-                scan_res = requests.post(
-                    "https://www.virustotal.com/api/v3/files",
-                    headers=headers,
-                    files={
-                        "file": (path, file)
-                    })
-                data_src = json.loads(scan_res.text)
-                data_url = data_src["data"]["links"]["self"]
-                result_res = requests.get(data_url, headers=headers)
-                print(result_res.text)
+    root_path = r"C:\Users\abish\Downloads\toscan"
+    if "download_state" and "path" in request.json:
+        print(root_path+ " "+ request.json["path"])
+        if root_path in request.json["path"]:
+            download_path = request.json["path"]
+            print(f"Yeees, download complete at {download_path}")
+        else:
+            print("Noooooo, my plan has failed")
     return jsonify({}), 200
+
+
 
 
 def main() -> None:
