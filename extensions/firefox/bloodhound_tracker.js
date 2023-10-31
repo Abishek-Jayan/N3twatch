10454f4b315bcf3fa6abe79a6ba12645378ee9c7
Abishek <abishekjayan98@gmail.com>
2023-10-31 11:47:13 +0530

    Updated browser extension

--- extensions/firefox/bloodhound_tracker.js
+++ extensions/firefox/bloodhound_tracker.js
@@ -6,17 +6,20 @@ browser.downloads.onCreated.addListener((e) => {
 browser.downloads.onChanged.addListener((e) => {
   if (e.state.current === "complete") {
     if (download_list[e.id]) {
-      let request = new Request("http://localhost:5000/dwnld_state", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          path: download_list[e.id],
-          download_state: e.state.current,
-          download_id: e.id,
-        }),
-      });
+      let request = new Request(
+        "https://n3twatch-qx9u4c0k6-abishek-jayan.vercel.app/dwnld_state",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            path: download_list[e.id],
+            download_state: e.state.current,
+            download_id: e.id,
+          }),
+        }
+      );
       fetch(request).catch((error) => {
         console.error(error);
       });
