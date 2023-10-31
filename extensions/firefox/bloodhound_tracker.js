1ed36c388deef1f51a5aa9e405476a50df967aae
Abishek <abishekjayan98@gmail.com>
2023-10-31 13:06:59 +0530

    Removed flask dependency and focusing purely on extension

--- extensions/firefox/bloodhound_tracker.js
+++ extensions/firefox/bloodhound_tracker.js
@@ -1,30 +1,67 @@
-let download_list = {};
+API_KEY = "hb2tx1w4131aad44gz56y9d019462476wuatj4ra389c25897bqgmr085d489ab1";
+API_SUMMARY_URL = "https://www.hybrid-analysis.com/api/v2/overview/";
+API_UPLOAD_URL = "https://www.hybrid-analysis.com/api/v2/quick-scan/url";
 
-browser.downloads.onCreated.addListener((e) => {
-  download_list[e.id] = e.filename;
-});
-browser.downloads.onChanged.addListener((e) => {
-  if (e.state.current === "complete") {
-    if (download_list[e.id]) {
-      let request = new Request(
-        "https://n3twatch-qx9u4c0k6-abishek-jayan.vercel.app/dwnld_state",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            path: download_list[e.id],
-            download_state: e.state.current,
-            download_id: e.id,
-          }),
-        }
-      );
-      fetch(request).catch((error) => {
-        console.error(error);
+browser.downloads.onCreated.addListener(async (e) => {
+  //Pause the download here
+  console.log(e);
+  browser.downloads.pause(e.id);
+  console.log("Paused");
+  const headers = {
+    "api-key": API_KEY,
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  };
+  const data = {
+    scan_type: "all",
+    url: e.url,
+    no_share_third_party: "",
+    allow_community_access: "",
+    comment: "",
+    submit_name: "",
+  };
+
+  // Perform the POST request using the Fetch API
+  try {
+    const response = await fetch(API_UPLOAD_URL, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      const sha = responseData.sha256;
+
+      // Make a GET request to retrieve the summary
+      const summaryResponse = await fetch(`${API_SUMMARY_URL}${sha}/summary`, {
+        headers: headers,
       });
 
-      delete download_list[e.id];
+      if (summaryResponse.ok) {
+        const summaryData = await summaryResponse.json();
+        if (summaryData.threat_score) {
+          console.log("Kill it with Fire!!!");
+          browser.downloads.cancel(e.id);
+        } else {
+          console.log("All clear");
+          browser.downloads.resume(e.id);
+        }
+      } else {
+        console.log(
+          "Failed to fetch summary:",
+          summaryResponse.status,
+          summaryResponse.statusText
+        );
+      }
+    } else {
+      console.log(
+        "Failed to upload the URL:",
+        response.status,
+        response.statusText
+      );
     }
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
 });
