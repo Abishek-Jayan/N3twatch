8828f775c257c2fcd26d784033787a6786840386
Abishek <abishekjayan98@gmail.com>
2023-11-07 12:32:53 +0530

    Completed N3twatch phase 1

--- TODO.md
+++ TODO.md
@@ -4,11 +4,10 @@
 - [x] Set up Flask server to recieve data from extensions
 - [x] Set up scanning from specific folder functionality
 - [x] Set up input for updating safe folder in webpage
-- [ ] Set up backend on Vercel
-- [ ] Set up extension to be permanent on browser
+- [x] Set up folder scanning and moving functionality
+- [x] Set up extension to be permanent on browser
 - [ ] Set up modular interface for creating custom virus scanner
 - [ ] Set up API to Polyswarm
-- [ ] Set up WSGI production server
 - [ ] Set up auto start
 - [ ] Set up installation interface
 - [ ] Set up link reader on mouse click
--- extensions/firefox/bloodhound_tracker.js
+++ extensions/firefox/bloodhound_tracker.js
@@ -1,59 +1,75 @@
 API_KEY = "hb2tx1w4131aad44gz56y9d019462476wuatj4ra389c25897bqgmr085d489ab1";
 API_SUMMARY_URL = "https://www.hybrid-analysis.com/api/v2/overview/";
 API_UPLOAD_URL = "https://www.hybrid-analysis.com/api/v2/quick-scan/url";
+TO_SCAN_DIR = "/home/abishek/Desktop/to_scan/";
 
 browser.downloads.onCreated.addListener(async (e) => {
   //Pause the download here
-  await browser.downloads.pause(e.id);
+  if (e.filename.includes(TO_SCAN_DIR)) {
+    await browser.downloads.pause(e.id);
 
-  const headers = {
-    "api-key": API_KEY,
-    Accept: "*/*",
-  };
+    const headers = {
+      "api-key": API_KEY,
+      Accept: "*/*",
+    };
 
-  const formdata = new FormData();
-  formdata.append("scan_type", "all");
-  formdata.append("url", e.url.trim());
+    const formdata = new FormData();
+    formdata.append("scan_type", "all");
+    formdata.append("url", e.url.trim());
 
-  // Perform the POST request using the Fetch API
-  try {
-    const response = await fetch(API_UPLOAD_URL, {
-      method: "POST",
-      headers: headers,
-      body: formdata,
-    });
-    if (response.ok) {
-      const responseData = await response.json();
-      const sha = responseData.sha256;
-
-      // Make a GET request to retrieve the summary
-      const summaryResponse = await fetch(`${API_SUMMARY_URL}${sha}/summary`, {
+    // Perform the POST request using the Fetch API
+    try {
+      const response = await fetch(API_UPLOAD_URL, {
+        method: "POST",
         headers: headers,
+        body: formdata,
       });
+      if (response.ok) {
+        const responseData = await response.json();
+        const sha = responseData.sha256;
 
-      if (summaryResponse.ok) {
-        const summaryData = await summaryResponse.json();
-        if (summaryData.threat_score) {
-          console.log("Kill it with Fire!!!");
+        // Make a GET request to retrieve the summary
+        const summaryResponse = await fetch(
+          `${API_SUMMARY_URL}${sha}/summary`,
+          {
+            headers: headers,
+          }
+        );
+
+        if (summaryResponse.ok) {
+          const summaryData = await summaryResponse.json();
+          if (summaryData.threat_score) {
+            console.log("Kill it with Fire!!!");
+            await browser.downloads.cancel(e.id);
+          } else {
+            console.log("All clear");
+            await browser.downloads.resume(e.id);
+          }
         } else {
-          console.log("All clear");
-          await browser.downloads.resume(e.id);
+          console.log(
+            "Failed to fetch summary:",
+            summaryResponse.status,
+            summaryResponse.statusText
+          );
         }
       } else {
         console.log(
-          "Failed to fetch summary:",
-          summaryResponse.status,
-          summaryResponse.statusText
+          "Failed to upload the URL:",
+          response.status,
+          response.statusText
         );
+        browser.downloads.cancel(e.id);
       }
-    } else {
-      console.log(
-        "Failed to upload the URL:",
-        response.status,
-        response.statusText
-      );
+    } catch (error) {
+      console.error("An error occurred:", error);
     }
-  } catch (error) {
-    console.error("An error occurred:", error);
   }
 });
+function getFileName(url) {
+  const [...parts] = url.split("/");
+
+  // get the filename
+  const fileName = parts.pop();
+
+  return fileName;
+}
